7c746093b0680d3c2ec2e9e1f28ed859a107ccef
Gal Noy <gal@tikalk.com>
2016-08-11 12:31:14 +0300

    remove token

--- src/State.js
+++ src/State.js
@@ -23,7 +23,7 @@ State.on('loadDevices', () => {
 const headers = new Headers({
   'Content-Type': 'application/json',
   'appId': 'b799aae9-51b8-4613-8a1d-894a80b35481',
-  'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImVtc3lzQGF4b25pemUuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInRlbmFudElkIjoiNTc0NTZjMGUxODZhMjgxOGU0ZjE4MWRlIiwiYXBwSWQiOiJiNzk5YWFlOS01MWI4LTQ2MTMtOGExZC04OTRhODBiMzU0ODEiLCJyb2xlcyI6WyJBZG1pbiJdLCJ1c2VySWQiOiI1NzQ1NmMxMDE4NmEyODE4ZTRmMTgxZTAiLCJpc3MiOiJodHRwczovL3NvaW90LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1NzQ1NmMxMTk3YWY2ZmZiMzAxYjRiYjkiLCJhdWQiOiIxQ0Z4UUJpYmpuTEFlWTdPTFVXam1aV041NlFLYzBIOSIsImV4cCI6MTQ3MDkzNjcxMiwiaWF0IjoxNDcwOTAwNzEyfQ.aFqxtxmxyupoXJDQ0ngA91Ht0SK-ZqfOe18YMymUdtQ'
+  'Authorization': 'Fill me up'
 });
 
 export default State
